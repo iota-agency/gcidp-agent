7c862f96467938a1c1de7e12649d6c6989733ee9
Diyor Khaydarov <diyor28july@gmail.com>
2024-01-14 17:36:17 +0500

    fix networks attach

--- docker/run.go
+++ docker/run.go
@@ -40,6 +40,7 @@ func (d *RunCommand) Run(ctx *pipeline.StageContext) error {
 	var endpointsConfig map[string]*dockerNetwork.EndpointSettings
 	if d.networkConfig != nil && d.networkConfig.EndpointsConfig != nil {
 		endpointsConfig = d.networkConfig.EndpointsConfig
+		d.networkConfig.EndpointsConfig = nil
 	}
 	resp, err := ctx.Client.ContainerCreate(context.Background(), d.config, d.hostConfig, d.networkConfig, nil, d.cName)
 	if err != nil {
